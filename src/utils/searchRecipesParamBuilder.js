111c3654198d2b95c3291f78055d8be1a92b9cd2
April Dae Bation <adaecb@gmail.com>
2025-04-29 15:02:24 +1000

    backend: update filters based on UI changes

--- src/utils/searchRecipesParamBuilder.js
+++ src/utils/searchRecipesParamBuilder.js
@@ -2,6 +2,11 @@ import { TAG_MAPPINGS, MEAL_TYPES } from "../constants/recipeFilters.js";
 
 export const searchRecipesParamBuilder = (category, tags) => {
     const params = new URLSearchParams();
+    const diets = [];
+    const intolerances = [];
+    const excludeIngredients = [];
+    const nutritionConstraints = {};
+    const keywords = [];
 
     if (category) {
         if (MEAL_TYPES.includes(category)) {
@@ -11,23 +16,55 @@ export const searchRecipesParamBuilder = (category, tags) => {
         }
     }
 
-    const intolerances = (tags ? tags.split(",") : []).reduce((acc, tag) => {
+    (tags ? tags.split(",") : []).forEach((tag) => {
         const mapping = TAG_MAPPINGS[tag.trim()];
         if (mapping) {
-            if (mapping.param === "intolerances") {
-                acc.push(mapping.value);
-            } else {
-                params.append(mapping.param, mapping.value);
+            switch (mapping.param) {
+                case "diet":
+                    diets.push(mapping.value);
+                    break;
+                case "intolerances":
+                    intolerances.push(mapping.value);
+                    break;
+                case "excludeIngredients":
+                    excludeIngredients.push(mapping.value);
+                    break;
+                case "nutrition":
+                    nutritionConstraints[mapping.key] = mapping.value;
+                    break;
+                case "keyword":
+                    keywords.push(mapping.value);
+                    break;
+                default:
+                    params.append(mapping.param, mapping.value);
+                    break;
             }
         }
-        return acc;
-    }, []);
+    });
+
+    if (diets.length > 0) {
+        params.append("diet", diets.join(","));
+    }
 
     if (intolerances.length > 0) {
         params.append("intolerances", intolerances.join(","));
     }
 
-    // TODO: Add allergies after user settings is implemented
+    if (excludeIngredients.length > 0) {
+        params.append("excludeIngredients", excludeIngredients.join(","));
+    }
+
+    if (Object.keys(nutritionConstraints).length > 0) {
+        for (const [fullKey, value] of Object.entries(nutritionConstraints)) {
+            params.append(fullKey, value);
+        }
+    }
+
+    if (keywords.length > 0) {
+        params.append("query", keywords.join(","));
+    }
+
+    // TODO: Add allergies from user profile settings later if needed
 
     return params;
 };

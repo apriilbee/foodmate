cf8a780efb1e3e53c14d75968bae632aaca0e43c
April Dae Bation <adaecb@gmail.com>
2025-05-05 14:48:04 +1000

    frontend: linter issue fix

--- public/js/mealModal.js
+++ public/js/mealModal.js
@@ -45,3 +45,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
 });
+
+window.openModal = openModal;
+window.closeModal = closeModal;
